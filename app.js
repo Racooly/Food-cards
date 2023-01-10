9700c79c1bab2d3b52d24e7d4fb066d000f34174
sandro <sandro@gmail.com>
2023-01-11 01:39:06 +0400

    search bar added

---
+++ app.js
@@ -0,0 +1,12 @@
+function search(){
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase()
+    let Name = document.querySelectorAll('#Name')
+    for(let i = 0 ; i < Name.length ; i++){
+        if(!Name[i].innerHTML.includes(input)){
+        Name[i].style.display = "none"
+    }else{
+        Name[i].style.display = "inline"
+    }
+  }
+}
